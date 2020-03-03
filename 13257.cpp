0cb2adebb6851368040d80b6ebc5d991a7886ea9
WaywayBao <55104237+WaywayBao@users.noreply.github.com>
2020-03-04 00:43:03 +0800

    Update 13257.cpp

--- 13257.cpp
+++ 13257.cpp
@@ -1,37 +1,49 @@
 #include<iostream>
-#include<string>
+#include<cstring>
+#define N 26
+#define MAX 100005
 using namespace std;
+
+int dp[MAX][N];
+int len, cnt;
+
+//solved by recursive
+//void search(int index, int rec){
+//	for(int i=0; i<N; ++i){
+//		if(dp[index][i] < len){
+//			if(rec == 2) ++cnt;
+//			else search(dp[index][i]+1, rec+1);
+//		}
+//	}
+//}
 
 int main(){
     int T;
+    string str;
     cin >> T;
     while(T--){
-	    string str;
         cin >> str;
-        int len = str.length(), cnt = 0;
-        int front[30] = {};
-		int back[30] = {};
-        for(int i=0; i<30; ++i) front[i] = 999999, back[i] = -1;
-        for(int i=0, j=len-1; i<len; ++i, --j){
-            if(front[str[i]-'A'] == 999999) front[str[i]-'A'] = i;
-            if(back[str[j]-'A'] == -1) back[str[j]-'A'] = j;
+        len = str.length(), cnt = 0;
+
+		for(int i=0; i<N; ++i)
+			dp[len][i] = len;
+        for(int i=len-1; i>=0; --i){
+			memcpy(dp[i], dp[i+1], sizeof(dp[0]));
+			dp[i][str[i]-'A'] = i;
         }
-        for(int i=0; i<26; ++i){
-            if(front[i] != 999999){
-            	int flag1 = front[i];
-                for(int j=0; j<26; ++j){
-                	if(back[j] <= flag1) continue;
-                    int flag2 = 0;
-                    for(flag2 = flag1+1; flag2 < len; ++flag2){
-			    	    if(str[flag2] == (j+'A')) break;
-				    }
-                    if(flag2 != len){
-                        for(int k=0; k<26; ++k)
-			               	if(back[k] > flag2) ++cnt;
-                	}
-                }
-            }
-        }
+        for(int i=0; i<N; ++i){
+			if(dp[0][i] < len){
+				int x = dp[0][i] + 1;
+				for(int j=0; j<N; ++j){
+					if(dp[x][j] < len){
+						int y = dp[x][j] + 1;
+						for(int k=0; k<N; ++k)
+							if(dp[y][k] < len)	++cnt;
+					}
+				}
+			}
+		}
+        //search(0,0);
         cout<< cnt <<endl;
     }
     return 0;
